78caa0490c691a016f51f60869123a1e593ab43d
Yadnyesh23 <yadnyeshhalde2006@gmail.com>
2026-07-29 11:18:48 +0530

    phase 7.0 Authentication and Authorization

--- Projects/TaskManagement/app/domain/enums/db.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class TaskStatus(str, Enum):
-    PENDING = "pending"
-    COMPLETED = "completed"
-    
--- Projects/TaskManagement/app/domain/schemas/responses/health.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-class HealthCheckResponse(BaseModel):
-    status : str
-    database : str
-    message : str
-    timestamp : datetime
-    
